f97672ffbeed02317cb7598d6e94fc3a09656286
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-01-16 22:41:15 +0200

    update

---
+++ HandMade.Entities/ViewModels/AddRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandMade.Entities.ViewModels
+{
+    public class AddRoleViewModel
+    {
+        [Required]
+
+        public string RoleName { get; set; }
+    }
+}
---
+++ HandMade.Web/HandMade.Web/Areas/Identity/Controllers/RoleController.cs
@@ -0,0 +1,60 @@
+﻿using HandMade.Entities.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandMade.Web.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    [Authorize(Roles ="Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<string> roles = new List<string>();
+            if(roleManager.Roles != null)
+            {
+                foreach (var role in this.roleManager.Roles)
+                {
+                    roles.Add(role.Name);
+                }
+
+            }
+            
+            return View("Index",roles);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View("AddRole");
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(AddRoleViewModel roleVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleVM.RoleName);
+                if (roleExist == false)
+                {
+                    IdentityRole role = new IdentityRole();
+                    role.Name = roleVM.RoleName;
+
+                    IdentityResult result = await roleManager.CreateAsync(role);
+                    if (result == IdentityResult.Success)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View("AddRole",roleVM);
+        }
+    }
+}
