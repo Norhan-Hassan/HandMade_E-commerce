9fc480243dcd328c5ddb6f39704a2220242607b0
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-02-01 02:51:18 +0200

    .

---
+++ HandMade.DataAccess/Migrations/20250201002128_sh.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandMade.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class sh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    count = table.Column<int>(type: "int", nullable: false),
+             
+                    userId= table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    productId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.ID);
+                    table.ForeignKey(
+                        name: "productId",
+                        column: x => x.productId,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    
+                    table.ForeignKey(
+                        name: "userId",
+                        column: x => x.userId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+        }
+    }
+}
